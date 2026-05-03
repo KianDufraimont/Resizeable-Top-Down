de35f32d2d1650e54e9b37d927aaa3f53051be8c
Kian Dufraimont <kdufraim@uwo.ca>
2026-05-03 13:10:01 -0400

    Messed around with keybindings and moved movement into the player's code

--- Player.java
+++ Player.java
@@ -3,12 +3,16 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class Player{
-    private BufferedImage playerPic;
+    private final BufferedImage playerPic;
     private int tileIndex = 0;
     private int x, y, width, height;
-    public Player(BufferedImage pic, int startingTile){
+    private final int rows, columns;
+    
+    public Player(BufferedImage pic, int startingTile, int row, int column){
         playerPic = pic;
         tileIndex = startingTile;
+        rows = row;
+        columns = column;
         //setDrawInfo(0, 0, 100, 100);
     }
     
@@ -20,14 +24,30 @@ public class Player{
         return tileIndex;
     }
 
-    public void addTileCount(int count){
+    /*public void addTileCount(int count){
         tileIndex += count;
-    }
+    }*/
 
     public void setDrawInfo(int x, int y, int width, int height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    public enum Direction{
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+
+    public void movePlayer(Direction dir){
+        switch(dir){
+            case UP -> {if (tileIndex - rows >= 0) tileIndex-=rows;}
+            case DOWN -> { if (tileIndex + rows < rows*columns) tileIndex += rows; }
+            case LEFT -> {if (tileIndex % rows != 0) tileIndex -= 1;}
+            case RIGHT -> {if ((tileIndex + 1) % rows != 0) tileIndex += 1;}
+        }
     }
 }
--- World.java
+++ World.java
@@ -2,6 +2,7 @@
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
@@ -10,7 +11,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class World extends JPanel{
     private Player player, player2;
@@ -20,8 +26,8 @@ public class World extends JPanel{
         this.setLayout(new GridLayout(10,10,1,1));
         try {
             BufferedImage spriteSheet = ImageIO.read(new File("basictiles.png"));
-            BufferedImage tile1 = loadFromSpriteSheet(spriteSheet, 16, 2, 2);
-            BufferedImage tile2 = loadFromSpriteSheet(spriteSheet, 16, 4, 2);
+            BufferedImage tile1 = loadFromSpriteSheet(spriteSheet, 16, 0, 1);
+            BufferedImage tile2 = loadFromSpriteSheet(spriteSheet, 16, 1, 2);
 
             for (int i = 0; i < 10; i++){
                 for (int j = 0; j < 10; j++){
@@ -37,9 +43,12 @@ public class World extends JPanel{
 
             BufferedImage sheet = ImageIO.read(new File("characters.png"));
             BufferedImage playerPic = sheet.getSubimage(16*1, 0, 16, 16);
-            player = new Player(playerPic,0);
+            player = new Player(playerPic,0,10,10);
             playerPic = sheet.getSubimage(16*7, 0, 16, 16);
-            player2 = new Player(playerPic,99);
+            player2 = new Player(playerPic,99,10,10);
+
+            keyListener();
+            
             this.addComponentListener(new ComponentAdapter() {
                 @Override
                 public void componentResized(ComponentEvent e){
@@ -48,49 +57,7 @@ public class World extends JPanel{
                     repaint();
                 }
             });
-            this.addKeyListener(new KeyListener() {
-                @Override
-                public void keyTyped(KeyEvent e) {
-                }
-
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    int key = e.getKeyCode();
-                    switch(key){
-                        case KeyEvent.VK_DOWN:
-                            if (player2.getTileIndex() + 10 < 100) player2.addTileCount(10);
-                            break;
-                        case KeyEvent.VK_S:
-                            if (player.getTileIndex() + 10 < 100) player.addTileCount(10);
-                            break;
-                        case KeyEvent.VK_UP:
-                            if (player2.getTileIndex() - 10 >= 0) player2.addTileCount(-10);
-                            break;
-                        case KeyEvent.VK_W:
-                            if (player.getTileIndex() - 10 >= 0) player.addTileCount(-10);
-                            break;
-                        case KeyEvent.VK_LEFT:
-                            if ((player2.getTileIndex() % 10) != 0) player2.addTileCount(-1);
-                            break;
-                        case KeyEvent.VK_A:
-                            if (player.getTileIndex() - 1 >= 0) player.addTileCount(-1);
-                            break;
-                        case KeyEvent.VK_RIGHT:
-                            if ((player2.getTileIndex() + 1) %10 != 0) player2.addTileCount(1);
-                            break;
-                        case KeyEvent.VK_D:
-                            if (player.getTileIndex() + 1 < 100) player.addTileCount(1);
-                            break;
-                    }
-                    setPlayerLocation();
-                    repaint();
-                }
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-                }
             
-            });
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -124,5 +91,70 @@ public class World extends JPanel{
         super.paint(g);
         player.draw(g);
         player2.draw(g);
+    }
+
+    /**
+     * Makes a key listener and attaches player movement to different keys
+     */
+    private void keyListener(){
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                switch(key){
+                    case KeyEvent.VK_UP -> player2.movePlayer(Player.Direction.UP);
+                    case KeyEvent.VK_DOWN -> player2.movePlayer(Player.Direction.DOWN);
+                    case KeyEvent.VK_LEFT -> player2.movePlayer(Player.Direction.LEFT);
+                    case KeyEvent.VK_RIGHT -> player2.movePlayer(Player.Direction.RIGHT);
+
+                    case KeyEvent.VK_W -> player.movePlayer(Player.Direction.UP);
+                    case KeyEvent.VK_S -> player.movePlayer(Player.Direction.DOWN);                    
+                    case KeyEvent.VK_A -> player.movePlayer(Player.Direction.LEFT);
+                    case KeyEvent.VK_D -> player.movePlayer(Player.Direction.RIGHT);
+                }
+                setPlayerLocation();
+                repaint();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+            
+        });
+    }
+
+    //Work in Progress just to compare the differences between key listener and key bindings
+    private void keyBindings(){
+        InputMap inputMap = this.getInputMap(WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = this.getActionMap();
+        addKeyToActionMap(inputMap, actionMap, player, KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), Player.Direction.DOWN);
+        addKeyToActionMap(inputMap, actionMap, player, KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), Player.Direction.UP);
+        addKeyToActionMap(inputMap, actionMap, player, KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), Player.Direction.LEFT);
+        addKeyToActionMap(inputMap, actionMap, player, KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), Player.Direction.RIGHT);
+
+        addKeyToActionMap(inputMap, actionMap, player2, KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), Player.Direction.DOWN);
+        addKeyToActionMap(inputMap, actionMap, player2, KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), Player.Direction.UP);
+        addKeyToActionMap(inputMap, actionMap, player2, KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), Player.Direction.LEFT);
+        addKeyToActionMap(inputMap, actionMap, player2, KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), Player.Direction.RIGHT);
+
+    }
+
+    private void addKeyToActionMap(InputMap input, ActionMap action, Player pl, KeyStroke ks, Player.Direction dir){
+        Action ac = new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pl.movePlayer(dir);
+                setPlayerLocation();
+                repaint();
+            }
+            
+        };
+        System.out.println(ks.toString());
+        input.put(ks, ks.toString());
+        action.put(ks.toString(), ac);
     }
 }
